a845820a2a102451b2b7aa3f12b67bfa16d99c4f
Andy Lu <andy@stitchdata.com>
2019-04-05 21:09:56 +0000

    Fix MANIFEST, bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-recurly',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting data from the Recurly API',
       author='Stitch',
       url='http://github.com/singer-io/tap-recurly',
